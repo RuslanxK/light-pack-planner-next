67af6ed34b0775c521e85704cad1225644e731f6
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-06-24 19:57:41 +0300

    ASD

--- BL/emailBL.js
+++ BL/emailBL.js
@@ -7,7 +7,7 @@ export const generateRegisterHTML = (id) => {
         <h2 style="font-weight: 500; color: black";>Verify your email address</h2>
         <hr class="solid" style="width: 50%; border: 1px solid #ededed;">
         <p style="color: gray; font-size: 15px;">In order to use your Light Pack Planner account, you need to confirm your email address.</p>
-        <a href="${process.env.API_URL}/verify?id=${id}"><button style="padding: 15px; margin-bottom: 15px; border: none; color: white; width: 300px; background-color: #08CA28";>Verify your email address</button></a>
+        <a href="${process.env.EMAIL_URL}/verify?id=${id}"><button style="padding: 15px; margin-bottom: 15px; border: none; color: white; width: 300px; background-color: #08CA28";>Verify your email address</button></a>
         <hr class="solid" style="margin-bottom: 15px; width: 75px; border: 1px solid #ededed;">
         <span style="color: gray; font-size: 12px;">if you did not sign up for this account, you can ignore this email and the account will be deleted.</span>
       </div>
@@ -21,7 +21,7 @@ export const generateForgotPasswordHTML = (id) => {
         <h2 style="font-weight: 500; color: black";>Forgotten your password?</h2>
         <hr class="solid" style="width: 50%; border: 1px solid #ededed;">
         <p style="color: gray; font-size: 15px;">We're sending you this email because you requested a password reset. Click on this link to create a new password:</p>
-        <a href="${process.env.API_URL}/new-password?id=${id}"><button style="padding: 15px; margin-bottom: 15px; border: none; color: white; width: 300px; background-color: #08CA28";>Set a new password</button></a>
+        <a href="${process.env.EMAIL_URL}/new-password?id=${id}"><button style="padding: 15px; margin-bottom: 15px; border: none; color: white; width: 300px; background-color: #08CA28";>Set a new password</button></a>
         <hr class="solid" style="margin-bottom: 15px; width: 75px; border: 1px solid #ededed;">
         <span style="color: gray; font-size: 12px;">if you did not request a password reset, you can ignore this email. Your password will not be changed.</span>
       </div>
