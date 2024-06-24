3cbebf295c422f832703988ed3c2db13db1b3b5e
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-06-24 18:01:08 +0300

    jnj

--- components/ExploreBags.jsx
+++ components/ExploreBags.jsx
@@ -1,8 +1,26 @@
-"use client"
+"use client";
 
-
-import React, { useState } from 'react';
-import { Container, Typography, Stack, IconButton, Alert, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Link, TextField, MenuItem, FormControl, InputLabel, Select, Slider } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Typography,
+  Stack,
+  IconButton,
+  Alert,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Link,
+  TextField,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+  Slider
+} from '@mui/material';
 import { useTheme } from '@emotion/react';
 import { useRouter } from 'next/navigation';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
@@ -11,10 +29,12 @@ const ExploreBags = ({ exploreBags }) => {
   const theme = useTheme();
   const router = useRouter();
   const [searchFilter, setSearchFilter] = useState('');
-  const [categoryRange, setCategoryRange] = useState([0, 10]);
-  const [itemRange, setItemRange] = useState([0, 10]);
+  const [categoryRange, setCategoryRange] = useState([0, 100]);
+  const [itemRange, setItemRange] = useState([0, 200]);
   const [likesFilter, setLikesFilter] = useState('');
   const [weightFilter, setWeightFilter] = useState('');
+  const [weightUnitFilter, setWeightUnitFilter] = useState('');
+  const [weightOptions, setWeightOptions] = useState([]);
 
   const handleSliderChange = (filterSetter) => (e, newValue) => {
     filterSetter(newValue);
@@ -24,22 +44,55 @@ const ExploreBags = ({ exploreBags }) => {
     filterSetter(e.target.value);
   };
 
-  const filteredBags = exploreBags.filter((bag) => 
-    (bag.name.toLowerCase().includes(searchFilter.toLowerCase()) || bag.userDetails.username.toLowerCase().includes(searchFilter.toLowerCase())) &&
+  useEffect(() => {
+    const getWeightOptions = () => {
+      switch (weightUnitFilter) {
+        case 'kg':
+          return ['0-5', '5-10', '10-15', '15+'];
+        case 'lb':
+          return ['0-10', '10-20', '20-30', '30+'];
+        case 'g':
+          return ['0-5000', '5000-10000', '10000-15000', '15000+'];
+        case 'oz':
+          return ['0-175', '175-350', '350-525', '525+'];
+        default:
+          return ['Select weight unit'];
+      }
+    };
+    setWeightOptions(getWeightOptions());
+    setWeightFilter('');
+  }, [weightUnitFilter]);
+
+  const filteredBags = exploreBags.filter((bag) =>
+    (bag.name.toLowerCase().includes(searchFilter.toLowerCase()) ||
+      bag.userDetails.username.toLowerCase().includes(searchFilter.toLowerCase())) &&
     (bag.totalCategories >= categoryRange[0] && bag.totalCategories <= categoryRange[1]) &&
     (bag.totalItems >= itemRange[0] && bag.totalItems <= itemRange[1]) &&
-    (likesFilter === '' || 
-      (likesFilter === '0-10' && bag.likes >= 0 && bag.likes <= 10) || 
-      (likesFilter === '10-100' && bag.likes > 10 && bag.likes <= 100) || 
-      (likesFilter === '100-1000' && bag.likes > 100 && bag.likes <= 1000) || 
+    (likesFilter === '' ||
+      (likesFilter === '0-10' && bag.likes >= 0 && bag.likes <= 10) ||
+      (likesFilter === '10-100' && bag.likes > 10 && bag.likes <= 100) ||
+      (likesFilter === '100-1000' && bag.likes > 100 && bag.likes <= 1000) ||
       (likesFilter === '1000+' && bag.likes > 1000)
     ) &&
-    (weightFilter === '' || 
-      (weightFilter === '0-5' && bag.totalBagWeight >= 0 && bag.totalBagWeight <= 5) || 
-      (weightFilter === '5-10' && bag.totalBagWeight > 5 && bag.totalBagWeight <= 10) || 
-      (weightFilter === '10-15' && bag.totalBagWeight > 10 && bag.totalBagWeight <= 15) || 
-      (weightFilter === '15+' && bag.totalBagWeight > 15)
-    )
+    (weightFilter === '' ||
+      (weightFilter === '0-5' && bag.totalBagWeight >= 0 && bag.totalBagWeight <= 5 && weightUnitFilter === 'kg') ||
+      (weightFilter === '5-10' && bag.totalBagWeight > 5 && bag.totalBagWeight <= 10 && weightUnitFilter === 'kg') ||
+      (weightFilter === '10-15' && bag.totalBagWeight > 10 && bag.totalBagWeight <= 15 && weightUnitFilter === 'kg') ||
+      (weightFilter === '15+' && bag.totalBagWeight > 15 && weightUnitFilter === 'kg') ||
+      (weightFilter === '0-10' && bag.totalBagWeight >= 0 && bag.totalBagWeight <= 10 && weightUnitFilter === 'lb') ||
+      (weightFilter === '10-20' && bag.totalBagWeight > 10 && bag.totalBagWeight <= 20 && weightUnitFilter === 'lb') ||
+      (weightFilter === '20-30' && bag.totalBagWeight > 20 && bag.totalBagWeight <= 30 && weightUnitFilter === 'lb') ||
+      (weightFilter === '30+' && bag.totalBagWeight > 30 && weightUnitFilter === 'lb') ||
+      (weightFilter === '0-5000' && bag.totalBagWeight >= 0 && bag.totalBagWeight <= 5000 && weightUnitFilter === 'g') ||
+      (weightFilter === '5000-10000' && bag.totalBagWeight > 5000 && bag.totalBagWeight <= 10000 && weightUnitFilter === 'g') ||
+      (weightFilter === '10000-15000' && bag.totalBagWeight > 10000 && bag.totalBagWeight <= 15000 && weightUnitFilter === 'g') ||
+      (weightFilter === '15000+' && bag.totalBagWeight > 15000 && weightUnitFilter === 'g') ||
+      (weightFilter === '0-175' && bag.totalBagWeight >= 0 && bag.totalBagWeight <= 175 && weightUnitFilter === 'oz') ||
+      (weightFilter === '175-350' && bag.totalBagWeight > 175 && bag.totalBagWeight <= 350 && weightUnitFilter === 'oz') ||
+      (weightFilter === '350-525' && bag.totalBagWeight > 350 && bag.totalBagWeight <= 525 && weightUnitFilter === 'oz') ||
+      (weightFilter === '525+' && bag.totalBagWeight > 525 && weightUnitFilter === 'oz')
+    ) &&
+    (weightUnitFilter === '' || bag.userDetails.weightOption === weightUnitFilter)
   );
 
   return (
@@ -65,130 +118,135 @@ const ExploreBags = ({ exploreBags }) => {
               label="Search by Name or Owner"
               variant="outlined"
               size='small'
-              InputLabelProps={{ style : {fontSize: 14}}}
-              sx={{ width: "30%"}}
+              InputLabelProps={{ style: { fontSize: 14 } }}
+              sx={{ width: "30%" }}
               value={searchFilter}
               onChange={(e) => setSearchFilter(e.target.value)}
             />
 
             <FormControl variant="outlined" size="small" sx={{ width: "8%" }}>
-              <InputLabel sx={{fontSize: "14px"}}>Likes</InputLabel>
+              <InputLabel sx={{ fontSize: "14px" }}>Likes</InputLabel>
               <Select
                 label="Likes"
                 value={likesFilter}
                 onChange={handleSelectChange(setLikesFilter)}
               >
-                <MenuItem sx={{fontSize: "14px"}} value="">All</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}} value="0-10">0-10</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}} value="10-100">10-100</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}} value="100-1000">100-1000</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}} value="1000+">1000+</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="">All</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="0-10">0-10</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="10-100">10-100</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="100-1000">100-1000</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="1000+">1000+</MenuItem>
               </Select>
             </FormControl>
 
-            <FormControl variant="outlined" size="small" sx={{width: "12%" }}>
-              <InputLabel sx={{fontSize: "14px"}}>Total Weight</InputLabel>
+
+            <FormControl variant="outlined" size="small" sx={{ width: "10%" }}>
+              <InputLabel sx={{ fontSize: "14px" }}>Weight Unit</InputLabel>
+              <Select
+                label="Weight Unit"
+                value={weightUnitFilter}
+                sx={{ fontSize: "14px" }}
+                onChange={handleSelectChange(setWeightUnitFilter)}
+              >
+                <MenuItem sx={{ fontSize: "14px" }} value="">All</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="kg">kg</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="lb">lb</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="g">g</MenuItem>
+                <MenuItem sx={{ fontSize: "14px" }} value="oz">oz</MenuItem>
+              </Select>
+            </FormControl>
+
+            
+            <FormControl variant="outlined" size="small" sx={{ width: "12%" }}>
+              <InputLabel sx={{ fontSize: "14px" }}>Total Weight</InputLabel>
               <Select
                 label="Total Bag Weight"
                 value={weightFilter}
-                sx={{fontSize: "14px"}}
+                sx={{ fontSize: "14px" }}
                 onChange={handleSelectChange(setWeightFilter)}
               >
-                <MenuItem sx={{fontSize: "14px"}} value="">All</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}}value="0-5">0-5</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}}value="5-10">5-10</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}}value="10-15">10-15</MenuItem>
-                <MenuItem sx={{fontSize: "14px"}} value="15+">15+</MenuItem>
+                {weightOptions.map(option => (
+                  <MenuItem key={option} sx={{ fontSize: "14px" }} value={option}>{option}</MenuItem>
+                ))}
               </Select>
             </FormControl>
 
-            <Stack display="flex" justifyContent="stretch" width="15%">
-            <InputLabel sx={{fontSize: "14px"}}>Categories</InputLabel>
-              <FormControl variant="outlined" size="small" sx={{ width: "100%", paddingRight: '10px' }}>
-                <Slider
-                  value={categoryRange}
-                  onChange={handleSliderChange(setCategoryRange)}
-                  valueLabelDisplay="auto"
-                  min={0}
-                  max={100} // Adjust the max value based on your actual data
-                  step={1}
-                />
-              </FormControl>
+            <Stack direction="column" justifyContent="center" alignItems="center" width="10%">
+              <Typography variant="span" fontSize="12px" fontWeight={500} textAlign="center">
+                Categories: {categoryRange[0]} - {categoryRange[1]}
+              </Typography>
+              <Slider
+                value={categoryRange}
+                min={0}
+                max={100}
+                step={1}
+                valueLabelDisplay="auto"
+                onChange={handleSliderChange(setCategoryRange)}
+              />
             </Stack>
 
-            <Stack display="flex" justifyContent="stretch" width="15%">
-            <InputLabel sx={{fontSize: "14px"}}>Items</InputLabel>
-              <FormControl variant="outlined" size="small" sx={{ width: "100%" }}>
-                <Slider
-                  value={itemRange}
-                  onChange={handleSliderChange(setItemRange)}
-                  valueLabelDisplay="auto"
-                  min={0}
-                  max={200} // Adjust the max value based on your actual data
-                  step={1}
-                />
-              </FormControl>
+            <Stack direction="column" justifyContent="center" alignItems="center" width="10%">
+              <Typography variant="span" fontSize="12px" fontWeight={500} textAlign="center">
+                Items: {itemRange[0]} - {itemRange[1]}
+              </Typography>
+              <Slider
+                value={itemRange}
+                min={0}
+                max={200}
+                step={1}
+                valueLabelDisplay="auto"
+                onChange={handleSliderChange(setItemRange)}
+              />
             </Stack>
           </Stack>
+        </Stack>
 
-          {filteredBags.length === 0 ? (
-            <Alert severity="warning" sx={{ marginTop: '10px' }}>
-              There are no shared bags yet.
+        {filteredBags.length === 0 && (
+          <Stack pl={5} pr={5}>
+            <Alert severity="info">
+              <strong>No bags found.</strong>
+              <p>Try adjusting the filters or search criteria.</p>
             </Alert>
-          ) : (
+          </Stack>
+        )}
+
+        {filteredBags.length > 0 && (
+          <Stack pl={5} pr={5}>
             <TableContainer>
-              <Table
-                size="small"
-                sx={{
-                  minWidth: 650,
-                  boxShadow: "rgba(0, 0, 0, 0.15) 0px 2px 8px;",
-                  background: theme.palette.mode === "dark" ? theme.main.darkColor : "#FAFAFA",
-                  marginTop: "10px"
-                }}
-                aria-label="simple table"
-              >
-                <TableHead sx={{ background: theme.palette.mode === "dark" ? "#171717" : "#F2F2F2" }}>
+              <Table>
+                <TableHead>
                   <TableRow>
                     <TableCell>Name</TableCell>
-                    <TableCell align="left">Owner</TableCell>
-                    <TableCell align="center">Categories</TableCell>
-                    <TableCell align="center">Items</TableCell>
-                    <TableCell align="center">Total Weight</TableCell>
-                    <TableCell align="center">Likes</TableCell>
+                    <TableCell>Owner</TableCell>
+                    <TableCell>Categories</TableCell>
+                    <TableCell>Items</TableCell>
+                    <TableCell>Likes</TableCell>
+                    <TableCell>Weight</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {filteredBags.map((row) => (
-                    <TableRow
-                      key={row._id}
-                      sx={{
-                        '&:last-child td, &:last-child th': { border: 0 },
-                        '&:hover': {
-                          backgroundColor: theme.palette.mode === 'dark' ? '#6e6d6d' : "#e6e3e3",
-                          cursor: 'pointer'
-                        }
-                      }}
-                    >
-                      <TableCell component="th" scope="row">
-                        <Link href={`/share?id=${row._id}`} target="_blank" rel="noopener noreferrer" underline="none">
-                          {row.name}
+                  {filteredBags.map(bag => (
+                    <TableRow key={bag.id}>
+                      <TableCell>
+                        <Link href={`/bags/${bag.id}`} passHref>
+                          {bag.name}
                         </Link>
                       </TableCell>
-                      <TableCell align="left">{row.userDetails.username}</TableCell>
-                      <TableCell align="center">{row.totalCategories}</TableCell>
-                      <TableCell align="center">{row.totalItems}</TableCell>
-                      <TableCell align="center">{row.totalBagWeight.toFixed(2)} / {row.goal} {row.userDetails.weightOption}</TableCell>
-                      <TableCell align="center">{row.likes}</TableCell>
+                      <TableCell>{bag.userDetails.username}</TableCell>
+                      <TableCell>{bag.totalCategories}</TableCell>
+                      <TableCell>{bag.totalItems}</TableCell>
+                      <TableCell>{bag.likes}</TableCell>
+                      <TableCell>{`${bag.totalBagWeight.toFixed(2)} / ${bag.goal} ${bag.userDetails.weightOption}`}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
               </Table>
             </TableContainer>
-          )}
-        </Stack>
+          </Stack>
+        )}
       </Stack>
     </Container>
   );
-}
+};
 
 export default ExploreBags;
