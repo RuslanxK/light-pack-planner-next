f047e311d61c40fd7cf00defd3e9e2996f423993
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-06-26 18:05:19 +0300

    asd

--- components/SharedItem.jsx
+++ components/SharedItem.jsx
@@ -39,7 +39,7 @@ const SharedItem = (props) => {
     <div className="scroll-div">
     
       
-      <Stack  mb={0.5} pb={0.5} pl={1} pr={1} flexDirection="row" alignItems={theme.end}>
+      <div className='item-div'>
 
       {props.itemData.productImageKey ? <img src={`${process.env.NEXT_PUBLIC_PROFILE_URL}/${props.itemData.productImageKey}`} onClick={() => setPicPopupOpen(true)} style={{marginRight: "10px"}} width="50px" height="50px" alt='item'/> : null }
 
@@ -70,7 +70,7 @@ const SharedItem = (props) => {
             </Tooltip>
       
             
-          </Stack>
+          </div>
           
     
       
