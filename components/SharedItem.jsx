73036391826753249c69883ec66fc19a446a93f4
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-06-26 18:14:14 +0300

    asd

--- components/SharedItem.jsx
+++ components/SharedItem.jsx
@@ -44,7 +44,7 @@ const SharedItem = (props) => {
       {props.itemData.productImageKey ? <img src={`${process.env.NEXT_PUBLIC_PROFILE_URL}/${props.itemData.productImageKey}`} onClick={() => setPicPopupOpen(true)} style={{marginRight: "10px"}} width="50px" height="50px" alt='item'/> : null }
 
       {props.itemData.link ? (
-          <Link href={props.itemData.link} target="_blank" rel="noopener" underline="none" sx={{ width: '50%', marginRight: "15px", borderBottom: theme.palette.mode === "dark" ? `1px solid ${theme.main.darkColor}` : "1px solid #C0C0C0", fontSize: 12, color: theme.palette.info.main }}>
+          <Link href={props.itemData.link} target="_blank" rel="noopener" underline="none" sx={{ width: '50%', borderBottom: theme.palette.mode === "dark" ? `1px solid ${theme.main.darkColor}` : "1px solid #C0C0C0", fontSize: 12, color: theme.palette.info.main }}>
             {props.itemData.name}
           </Link>
         ) : (
@@ -53,11 +53,11 @@ const SharedItem = (props) => {
       <TextField size='small' variant='standard' placeholder='note' name='description' sx={{ width: '100%', marginRight: "15px", borderBottom: theme.palette.mode === "dark" ? `1px solid gray` : "1px solid #C0C0C0"}} value={props.itemData.description} InputLabelProps={{style : {fontSize: 12}}} inputProps={{readOnly: true, style: {fontSize: 12}}} InputProps={{disableUnderline: true}} />
       <TextField size='small' variant='standard' type='number' name='price' label="$ price" step="any" sx={{width: '12%', marginRight: "15px", borderBottom: theme.palette.mode === "dark" ? `1px solid gray` : "1px solid #C0C0C0"}} value={props.itemData.price} InputLabelProps={{ style : {fontSize: 12}}} InputProps={{disableUnderline: true, readOnly: true}} inputProps={{ min: 1, max: 99, style: {fontSize: 12} }} />
       <TextField size='small' variant='standard' type='number' name='qty' label="qty" sx={{width: '12%', marginRight: "15px", borderBottom: theme.palette.mode === "dark" ? `1px solid gray` : "1px solid #C0C0C0"}} value={props.itemData.qty} InputLabelProps={{ style : {fontSize: 12}}} InputProps={{disableUnderline: true, readOnly: true}} inputProps={{ min: 1, max: 99, style: {fontSize: 12} }} />
-      <TextField size='small' variant='standard' type='number' name='weight' label='weight' sx={{width: '12%', marginRight: "10px", borderBottom: theme.palette.mode === "dark" ? `1px solid gray` : "1px solid #C0C0C0"}} value={props.itemData.weight} InputLabelProps={{ style : {fontSize: 12}}} InputProps={{disableUnderline: true, readOnly: true}} inputProps={{ min: 0.1, max: 99, style: {fontSize: 12} }} />
+      <TextField size='small' variant='standard' type='number' name='weight' label='weight' sx={{width: '12%', marginRight: "15px", borderBottom: theme.palette.mode === "dark" ? `1px solid gray` : "1px solid #C0C0C0"}} value={props.itemData.weight} InputLabelProps={{ style : {fontSize: 12}}} InputProps={{disableUnderline: true, readOnly: true}} inputProps={{ min: 0.1, max: 99, style: {fontSize: 12} }} />
        <Typography fontSize="12px" variant='span' component="span" border="1px solid gray" borderRadius="3px" padding="7px">{props?.weightOption}</Typography>
 
       
-      <Select size='small' variant='outlined' disabled name='priority' sx={{width: '15%', fontSize: "12px",  marginRight: "10px", marginLeft: "15px", backgroundColor: getBackgroundColor(props.itemData.priority)}} value={props.itemData.priority}>  
+      <Select size='small' variant='outlined' disabled name='priority' sx={{width: '15%', fontSize: "12px",  marginRight: "15px", marginLeft: "15px", backgroundColor: getBackgroundColor(props.itemData.priority)}} value={props.itemData.priority}>  
        <MenuItem sx={{fontSize: "12px"}} value="low">Low priority</MenuItem>
        <MenuItem sx={{fontSize: "12px"}} value="medium">Med priority</MenuItem>
        <MenuItem sx={{fontSize: "12px"}} value="high">High priority</MenuItem>
