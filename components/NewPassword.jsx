b37a9412f3e59a3bf3ccd572c615a7c9529d410f
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-07-02 10:18:03 +0300

    asd

--- components/NewPassword.jsx
+++ components/NewPassword.jsx
@@ -90,7 +90,7 @@ const NewPassword = ({user, err}) => {
 
     <div className="login-content">
     <img id="logo-mobile-login" src="/logo.png" alt="logo" width="90px" height="58" style={{ position: "absolute", top: "25px", left: '25px' }}/> 
-    <Stack display={theme.flexBox} justifyContent={theme.center} alignItems="stretch" width="100%" p={12} sx={{ maxWidth: "800px" }}>
+    <div className='login-form'>
     <h1 className='login-text'>Reset your password</h1>
     <Typography component="span" variant="span" mb={4} width="100%" color="gray">
           Enter the email address you used to create the account, and we will email you instructions to reset your password
@@ -98,8 +98,8 @@ const NewPassword = ({user, err}) => {
     <form onSubmit={handleSubmit}>
     <div style={{display: "flex", marginBottom: "15px", flexDirection: "column", justifyContent: "center", alignItems: "stretch", borderRadius:"10px"}}>
     
-    <TextField type="password" required inputProps={{minLength : 10}} label="New Password" name="password" onChange={handleChange} sx={{marginBottom: "15px", background: "white", borderRadius: "7px"}} />
-    <TextField type="password" required inputProps={{minLength : 10}} label="Confirm New Password" name="confirmPassword" onChange={handleChange} sx={{marginBottom: "15px", background: "white", borderRadius: "7px"}} />
+    <TextField type="password" variant='filled' required inputProps={{minLength : 10}} label="New Password" name="password" onChange={handleChange} sx={{marginBottom: "15px", background: "white", borderRadius: "7px"}} />
+    <TextField type="password" variant='filled' required inputProps={{minLength : 10}} label="Confirm New Password" name="confirmPassword" onChange={handleChange} sx={{marginBottom: "15px", background: "white", borderRadius: "7px"}} />
     <button type='submit' className="login-button-regular" style={{display: "flex", justifyContent: "center"}}>Reset Password { isLoading ? <CircularProgress color="inherit" size={20} sx={{marginLeft: "15px"}} /> : null }</button>
     <Typography component="span" variant="span" width="100%" color="gray" sx={{cursor: "pointer"}}  mb={2}>Remember password? <Typography onClick={() => router.push("/login")} component="span" variant="span" color="#2d7fb5" sx={{cursor: "pointer"}}>Login</Typography></Typography>
 
@@ -110,7 +110,7 @@ const NewPassword = ({user, err}) => {
     { error ? <Alert severity="error">{error}</Alert> : null }
     {success ?  <Alert severity="success">{success}</Alert>: null }
 
-    </Stack>
+    </div>
     
 
       </div> </Fragment> }
