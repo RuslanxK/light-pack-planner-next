9acfe3a32999c4a8b69dff30ef88d791bb83edd7
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-07-02 10:45:34 +0300

    asd

--- components/NewPassword.jsx
+++ components/NewPassword.jsx
@@ -98,8 +98,8 @@ const NewPassword = ({user, err}) => {
     <form onSubmit={handleSubmit}>
     <div style={{display: "flex", marginBottom: "15px", flexDirection: "column", justifyContent: "center", alignItems: "stretch", borderRadius:"10px"}}>
     
-    <TextField type="password" variant='filled' required inputProps={{minLength : 10}} label="New Password" name="password" onChange={handleChange} sx={{marginBottom: "15px", background: "white", borderRadius: "7px"}} />
-    <TextField type="password" variant='filled' required inputProps={{minLength : 10}} label="Confirm New Password" name="confirmPassword" onChange={handleChange} sx={{marginBottom: "15px", background: "white", borderRadius: "7px"}} />
+    <TextField type="password" variant='filled' required inputProps={{minLength : 10}} label="New Password" name="password" onChange={handleChange} sx={{marginBottom: "15px"}} />
+    <TextField type="password" variant='filled' required inputProps={{minLength : 10}} label="Confirm New Password" name="confirmPassword" onChange={handleChange} sx={{marginBottom: "15px"}} />
     <button type='submit' className="login-button-regular" style={{display: "flex", justifyContent: "center"}}>Reset Password { isLoading ? <CircularProgress color="inherit" size={20} sx={{marginLeft: "15px"}} /> : null }</button>
     <Typography component="span" variant="span" width="100%" color="gray" sx={{cursor: "pointer"}}  mb={2}>Remember password? <Typography onClick={() => router.push("/login")} component="span" variant="span" color="#2d7fb5" sx={{cursor: "pointer"}}>Login</Typography></Typography>
 
